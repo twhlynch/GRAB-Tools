3bd99831eb01c9fcb2c13a754e2e775677c998c7
Tom Lynch <twhlynch@gmail.com>
2025-10-20 10:21:05 +1100

    feat: setup Tauri for desktop native webview builds
    rust btw

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue';
 import mkcert from 'vite-plugin-mkcert';
 import postcssNesting from 'postcss-nesting';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
 	plugins: [vue(), mkcert()],
 	resolve: {
@@ -40,13 +42,34 @@ export default defineConfig({
 	},
 	build: {
 		emptyOutDir: true,
+		target:
+			process.env.TAURI_ENV_PLATFORM == 'windows'
+				? 'chrome105'
+				: 'safari13',
+		minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+		sourcemap: !!process.env.TAURI_ENV_DEBUG,
 	},
 	server: {
 		https: true,
+		port: 5173,
+		strictPort: true,
+		host: host || false,
+		hmr: host
+			? {
+					protocol: 'ws',
+					host,
+					port: 1421,
+			  }
+			: undefined,
+		watch: {
+			ignored: ['**/src-tauri/**'],
+		},
 	},
+	envPrefix: ['VITE_', 'TAURI_ENV_*'],
 	css: {
 		postcss: {
 			plugins: [postcssNesting],
 		},
 	},
+	clearScreen: false,
 });
