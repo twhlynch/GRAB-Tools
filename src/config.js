7a8aca421af8fa10baf60597079a9b8593a38ea7
Tom Lynch <twhlynch@gmail.com>
2025-11-29 23:49:25 +1100

    feat: migrate to new api domain

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const GRAB_PAGE_URL = 'https://grabvr.quest/';
 export const LIVE_PAGE_URL = 'https://grabvr.tools/';
 export const STATS_SERVER_URL = 'https://grab-tools.live/stats_data/';
 export const DEV_PAGE_URL = 'https://localhost:5173/';
-export const SERVER_URL = 'https://grab-tools-api.twhlynch.workers.dev/';
+export const SERVER_URL = 'https://api.grabvr.tools/';
 export const LOGGING_URL = 'https://gt-logs.twhlynch.workers.dev';
 
 export const FORMAT_VERSION = 17;
