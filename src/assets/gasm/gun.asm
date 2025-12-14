c0d92dd86aeaba97e619eac1acc6d0794a67db5c
Tom Lynch <twhlynch@gmail.com>
2025-12-14 12:02:12 +1100

    feat: improve gun asm (still untested lol)

--- src/assets/gasm/gun.asm
+++ src/assets/gasm/gun.asm
@@ -1,52 +1,42 @@
 ;
 ;  gun.asm
 ;
-;  fires a projectile 'Laz' from a gun 'Gun' in its forwards direction
+;  fires a projectile 'Laz' from a gun 'Gun' in its local -z direction
 ;
 
-; set lazer to inside gun
+; teleport the lazer to inside gun and match its rotation
 
-SET Laz.Pos.X Gun.Pos.X
+SET Laz.Pos.X Gun.Pos.X ; set lazer position
 SET Laz.Pos.Y Gun.Pos.Y
 SET Laz.Pos.Z Gun.Pos.Z
 
-SET Laz.Rot.X Gun.Rot.X
+SET Laz.Rot.X Gun.Rot.X ; set lazer rotation
 SET Laz.Rot.Y Gun.Rot.Y
 SET Laz.Rot.Z Gun.Rot.Z
 
-; NOTE: GRAB rotation order is YXZ
+; GRAB rotation order is YXZ ( pitch yaw roll )
+; calculate forward direction ( local -z )
 
-; calculate forward direction
-; x =  cz * cy + sz * sx * sy
-; y =  cx * sy
-; z = -sz * cy + cz * sx * sy
+; x = -sx * cy
+; y = sy
+; z = -cx * cy
 
-COS R0 Laz.Rot.X ;  cx
-COS R1 Laz.Rot.Y ;  cy
-COS R2 Laz.Rot.Z ;  cz
-SIN R3 Laz.Rot.X ;  sx
-SIN R4 Laz.Rot.Y ;  sy
-SIN R5 Laz.Rot.Z ;  sz
-MUL R6 R5 -1     ; -sz
+COS R4 Laz.Rot.X ;  cx
+COS R5 Laz.Rot.Y ;  cy
+SIN R6 Laz.Rot.X ;  sx
+SIN R7 Laz.Rot.Y ;  sy
 
-MUL R7 R0 R4     ;  cx * sy
+MUL R6 R6 -1     ; -sx
+MUL R4 R4 -1     ; -cx
 
-MUL R0 R3 R4     ;  sx * sy
+MUL R0 R6 R5     ; x = -sx * cy
+SET R1 R7        ; y = sy
+MUL R2 R4 R5     ; z = -cx * cy
 
-MUL R3 R5 R0     ;  sz * sx * sy
-MUL R4 R2 R1     ;  cz * cy
-ADD R4 R4 R3     ;  cz * cy + sz * sx * sy
-
-MUL R3 R2 R0     ;  cz * sx * sy
-MUL R6 R6 R1     ; -sz * cy
-ADD R6 R6 R3     ; -sz * cy + cz * sx * sy
-
-SET R0 R4
-SET R1 R7
-SET R2 R6
-
-; normalise vector
-; x = x / sqrt(x^2 + y^2 + z^2)
+; normalise vector so its length is 1
+; this ensures diagonal directions arent longer and therefore faster
+; run the following on each of x y z
+; _ = _ / sqrt(x^2 + y^2 + z^2)
 
 MUL R3 R0 R0 ; x^2
 MUL R4 R1 R1 ; y^2
@@ -63,9 +53,9 @@ DIV R2 R2 R3 ; z / length
 
 ; apply speed
 
-SET R3 100 ; speed
+SET R3 100   ; speed constant
 
-MUL R0 R0 R3
+MUL R0 R0 R3 ; multiply x y z by speed
 MUL R1 R1 R3
 MUL R2 R2 R3
 
@@ -77,19 +67,23 @@ LABEL LOOP
 
     SET R6 DeltaTime
 
-    MUL R3 R0 R6
-    MUL R4 R1 R6
+    MUL R3 R0 R6 ; multiplying any movement by DeltaTime means it will
+    MUL R4 R1 R6 ; be consistent with long or short frames (from lag)
     MUL R5 R2 R6
+
+    ; gravity
+
+    MUL R7 R6 -9.81 ; 9.81 m/s down * DeltaTime
+    ADD R4 R4 R7    ; add to y movement
 
     ; add to lazer position to move it
 
-    ADD Laz.Pos.X R3 Laz.Pos.X
+    ADD Laz.Pos.X R3 Laz.Pos.X ; add the movement to the lazers position
     ADD Laz.Pos.Y R4 Laz.Pos.Y
     ADD Laz.Pos.Z R5 Laz.Pos.Z
 
     ; only run once per frame
 
-    SLEEP 0
+    SLEEP 0 ; sleeping for 0 will just prevent any more instructions running this frame
 
 GOTO LOOP
-
