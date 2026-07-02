f2842784b0716d56687c094057c7dc4629445154
TheTrueFax <130262457+TheTrueFax@users.noreply.github.com>
2026-07-02 18:26:13 +1200

    feat: finish all blankTypes, format

--- src/components/EditorPanels/PropertyPanel/MenuItem.vue
+++ src/components/EditorPanels/PropertyPanel/MenuItem.vue
@@ -1,9 +1,9 @@
 <script>
-import { defineComponent, ref } from 'vue';
 import { serializeToMenu } from '@/components/EditorPanels/PropertyPanel/menuSerializer';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-    emits: ['set', 'refresh'],
+	emits: ['set', 'refresh'],
 	props: {
 		node: Object,
 		reOpen: Boolean,
@@ -22,13 +22,13 @@ export default defineComponent({
 		return {
 			isExpanded: ref(false),
 			advancedEdit: ref(false),
-            isHovered: ref(false),
-            addMenuOpen: ref(false),
+			isHovered: ref(false),
+			addMenuOpen: ref(false),
 		};
 	},
 	methods: {
 		toggle(e) {
-            if (e.target.className.includes("modify-button")) return;
+			if (e.target.className.includes('modify-button')) return;
 			if (this.$props.node.isExpandable) {
 				this.isExpanded = !this.isExpanded;
 			}
@@ -36,63 +36,71 @@ export default defineComponent({
 		toggleAdvanced() {
 			this.advancedEdit = !this.advancedEdit;
 		},
-        addItemAbove() {
-            if (this.$props.node.arrayIndex==null) return;
-            console.log(this.$props.node);
-            this.$emit('set', (e) => {
-                console.log(e.$props.node);
-                //console.log(this.$props.node.blankTypes[this.$refs.blankTypeSelect.value]);
-                e.$props.node.children.splice(
-                    this.$props.node.arrayIndex, 
-                    0, 
-                    serializeToMenu(
-                        (Object.keys(this.$props.node.blankTypes).length==1)
-                            ? this.$props.node.blankTypes[Object.keys(this.$props.node.blankTypes)[0]]
-                            : this.$props.node.blankTypes[this.$refs.blankTypeSelect.value],
-                        this.$props.node.arrayIndex.toString(),
-                        this.$props.node.key,
-                        this.$props.node.arrayIndex
-                    )
-                );
-            });
-            this.$emit('refresh');
-        },
-        addItemBelow() {
-            if (this.$props.node.arrayIndex==null) return;
-            this.$emit('set', (e) => {
-                console.log(e.$props.node);
-                e.$props.node.children.splice(
-                    this.$props.node.arrayIndex-1, 
-                    0, 
-                    serializeToMenu(
-                        (Object.keys(this.$props.node.blankTypes).length==1)
-                            ? this.$props.node.blankTypes[Object.keys(this.$props.node.blankTypes)[0]]
-                            : this.$props.node.blankTypes[this.$refs.blankTypeSelect.value],
-                        this.$props.node.arrayIndex.toString(),
-                        this.$props.node.key,
-                        this.$props.node.arrayIndex
-                    )
-                );
-            });
-            this.$emit('refresh');
-        },
-        removeItem() {
-            if (this.$props.node.arrayIndex==null) return;
-            this.$emit('set', (e) => {
-                console.log(e.$props.node);
-                e.$props.node.children.splice(this.$props.node.arrayIndex, 1);
-                if (e.$props.node.children.length==0) {
-                    e.isExpanded=false;
-                }
-            });
-            this.$emit('refresh');
-        },
-        setEmit(e) {
-            e(this);
-        },
-        refreshEmit() {
-            this.$emit('refresh');
-        },
+		addItemAbove() {
+			if (this.$props.node.arrayIndex == null) return;
+			console.log(this.$props.node);
+			this.$emit('set', (e) => {
+				console.log(e.$props.node);
+				//console.log(this.$props.node.blankTypes[this.$refs.blankTypeSelect.value]);
+				e.$props.node.children.splice(
+					this.$props.node.arrayIndex,
+					0,
+					serializeToMenu(
+						Object.keys(this.$props.node.blankTypes).length == 1
+							? this.$props.node.blankTypes[
+									Object.keys(this.$props.node.blankTypes)[0]
+								]
+							: this.$props.node.blankTypes[
+									this.$refs.blankTypeSelect.value
+								],
+						this.$props.node.arrayIndex.toString(),
+						this.$props.node.key,
+						this.$props.node.arrayIndex,
+					),
+				);
+			});
+			this.$emit('refresh');
+		},
+		addItemBelow() {
+			if (this.$props.node.arrayIndex == null) return;
+			this.$emit('set', (e) => {
+				console.log(e.$props.node);
+				e.$props.node.children.splice(
+					this.$props.node.arrayIndex - 1,
+					0,
+					serializeToMenu(
+						Object.keys(this.$props.node.blankTypes).length == 1
+							? this.$props.node.blankTypes[
+									Object.keys(this.$props.node.blankTypes)[0]
+								]
+							: this.$props.node.blankTypes[
+									this.$refs.blankTypeSelect.value
+								],
+						this.$props.node.arrayIndex.toString(),
+						this.$props.node.key,
+						this.$props.node.arrayIndex,
+					),
+				);
+			});
+			this.$emit('refresh');
+		},
+		removeItem() {
+			if (this.$props.node.arrayIndex == null) return;
+			this.$emit('set', (e) => {
+				console.log(e.$props.node);
+				e.$props.node.children.splice(this.$props.node.arrayIndex, 1);
+				if (e.$props.node.children.length == 0) {
+					e.isExpanded = false;
+				}
+			});
+			this.$emit('refresh');
+		},
+		setEmit(e) {
+			e(this);
+		},
+		refreshEmit() {
+			this.$emit('refresh');
+		},
 	},
 });
 </script>
@@ -103,8 +111,8 @@ export default defineComponent({
 			class="menu-row"
 			:class="$props.node.isExpandable && 'clickable'"
 			@click="toggle"
-            @mouseenter="isHovered=true"
-            @mouseleave="isHovered=false"
+			@mouseenter="isHovered = true"
+			@mouseleave="isHovered = false"
 		>
 			<span v-if="$props.node.isExpandable" class="arrow">
 				{{ isExpanded ? 'v' : '>' }}
@@ -280,40 +288,59 @@ export default defineComponent({
 				>
 			</div>
 
-            <!-- Array modification buttons -->
-            <div v-if="isHovered && $props.node.arrayIndex!=null">
-                <span class="spacer"></span>
-                <button class="modify-button" @click="addMenuOpen = true">+</button>
-                <button class="modify-button red-button" @click="removeItem">X</button>
-            </div>
+			<!-- Array modification buttons -->
+			<div v-if="isHovered && $props.node.arrayIndex != null">
+				<span class="spacer"></span>
+				<button class="modify-button" @click="addMenuOpen = true">
+					+
+				</button>
+				<button class="modify-button red-button" @click="removeItem">
+					X
+				</button>
+			</div>
 		</div>
-        <div v-if="$props.node.arrayIndex!=null && addMenuOpen" class="add-menu" @mouseleave="addMenuOpen = false">
-            <span v-if="Object.keys($props.node.blankTypes).length > 1">
-                <select class="primitive-select primitive-input" ref="blankTypeSelect">
-                    <option
-                        v-for="(item, index) in Object.keys($props.node.blankTypes)"
-                        :value="item"
-                        :selected="index==0"
-                    >
-                        {{ item }}
-                    </option>
-                </select>
-                <br>
-            </span>
-            <button class="modify-button in-menu-button" @click="addItemAbove">Insert item above</button>
-            <br>
-            <button class="modify-button in-menu-button" @click="addItemBelow">Insert item below</button>
-        </div>
+		<div
+			v-if="$props.node.arrayIndex != null && addMenuOpen"
+			class="add-menu"
+			@mouseleave="addMenuOpen = false"
+		>
+			<span v-if="Object.keys($props.node.blankTypes).length > 1">
+				<select
+					class="primitive-select primitive-input"
+					ref="blankTypeSelect"
+				>
+					<option
+						v-for="(item, index) in Object.keys(
+							$props.node.blankTypes,
+						)"
+						:value="item"
+						:selected="index == 0"
+					>
+						{{ item }}
+					</option>
+				</select>
+				<br />
+			</span>
+			<button class="modify-button in-menu-button" @click="addItemAbove">
+				Insert item above
+			</button>
+			<br />
+			<button class="modify-button in-menu-button" @click="addItemBelow">
+				Insert item below
+			</button>
+		</div>
 		<div v-if="isExpanded" class="menu-children">
-            <span v-if="$props.node.key=='program'" class="node-key x-label">GASM not supported</span>
+			<span v-if="$props.node.key == 'program'" class="node-key x-label"
+				><span class="spacer"></span>GASM not supported</span
+			>
 			<MenuItem
-                v-else
+				v-else
 				v-for="child in $props.node.children"
 				:key="child.key"
 				:node="child"
 				:reOpen="$props.reOpen"
-                @set="setEmit"
-                @refresh="refreshEmit"
+				@set="setEmit"
+				@refresh="refreshEmit"
 			/>
 		</div>
 		<span
@@ -348,29 +375,29 @@ export default defineComponent({
 }
 
 .add-menu {
-    position: absolute;
-    padding:10px;
-    background: #141415;
-    border: 4px solid var(--border-color);
+	position: absolute;
+	padding: 10px;
+	background: #141415;
+	border: 4px solid var(--border-color);
 }
 
 .modify-button {
-    display:inline-block;
-    padding:5px;
-    border-radius:5px;
-    background: #3d3c7a;
-    color: #888;
-    cursor: pointer;
-    margin:2px;
+	display: inline-block;
+	padding: 5px;
+	border-radius: 5px;
+	background: #3d3c7a;
+	color: #888;
+	cursor: pointer;
+	margin: 2px;
 }
 .red-button {
-    background: #7a3c3c;
+	background: #7a3c3c;
 }
 
 .primitive-input {
 	background: var(--bg);
 	color: #aaa;
-    display:inline-block;
+	display: inline-block;
 }
 
 .primitive-number,
--- src/components/EditorPanels/PropertyPanel/menuSerializer.js
+++ src/components/EditorPanels/PropertyPanel/menuSerializer.js
@@ -7,15 +7,35 @@ function camelToTitleCase(str) {
 }
 
 const classify_as = {
-	vector3: ['position', 'scale', 'gradientDirection', 'direction', 'velocity', 'velocityMin', 'velocityMax', 'accelerationMin', 'accelerationMax'],
+	vector3: [
+		'position',
+		'scale',
+		'gradientDirection',
+		'direction',
+		'velocity',
+		'velocityMin',
+		'velocityMax',
+		'accelerationMin',
+		'accelerationMax',
+	],
 	vector4: ['rotation'],
-	color: ['color', 'color1', 'color2', 'startColor', 'endColor'],
-    minmax: ['lifeSpan', 'startSize', 'endSize'],
+	color: [
+		'color',
+		'color1',
+		'color2',
+		'startColor',
+		'endColor',
+		'skyColor0',
+		'skyColor1',
+		'sunColor',
+		'ambientColor',
+	],
+	minmax: ['lifeSpan', 'startSize', 'endSize'],
 };
 
 const enumerate_as = [
 	{
-		key: "shape",
+		key: 'shape',
 		enumData: [
 			[1000, 'Cube'],
 			[1001, 'Sphere'],
@@ -35,192 +55,269 @@ const enumerate_as = [
 			[8, 'Light'],
 		],
 	},
-    {
-	    key: "material",
-        enumData: [
-		    [0, 'Stone'],
-		    [1, 'Cheese'],
-		    [2, 'Ice'],
-		    [3, 'Lava'],
-		    [4, 'Wood'],
-		    [5, 'Grapple'],
-		    [6, 'Lava Grapple'],
-		    [7, 'Crumbling'],
-		    [8, 'Color / Concrete'],
-		    [9, 'Bouncing'],
-		    [10, 'Frost'],
-		    [11, 'Trigger'],
-        ]
+	{
+		key: 'material',
+		enumData: [
+			[0, 'Stone'],
+			[1, 'Cheese'],
+			[2, 'Ice'],
+			[3, 'Lava'],
+			[4, 'Wood'],
+			[5, 'Grapple'],
+			[6, 'Lava Grapple'],
+			[7, 'Crumbling'],
+			[8, 'Color / Concrete'],
+			[9, 'Bouncing'],
+			[10, 'Frost'],
+			[11, 'Trigger'],
+		],
 	},
 	{
-        key: "interpolationType",
-        enumData:[
-		[0, 'Linear'],
-		[1, 'Quadratic Ease In'],
-		[2, 'Quadratic Ease Out'],
-		[3, 'Quadratic Ease In Out'],
-		[4, 'Sinusoidal Ease In'],
-		[5, 'Sinusoidal Ease Out'],
-		[6, 'Sinusoidal Ease In Out'],
-		[7, 'Exponential Ease In'],
-		[8, 'Exponential Ease Out'],
-		[9, 'Exponential Ease In Out'],
-		[10, 'Circular Ease In'],
-		[11, 'Circular Ease Out'],
-		[12, 'Circular Ease In Out'],
-		[13, 'Cubic Ease In'],
-		[14, 'Cubic Ease Out'],
-		[15, 'Cubic Ease In Out'],
-		[16, 'Quartic Ease In'],
-		[17, 'Quartic Ease Out'],
-		[18, 'Quartic Ease In Out'],
-		[19, 'Quintic Ease In'],
-		[20, 'Quintic Ease Out'],
-		[21, 'Quintic Ease In Out'],
-	],
-    },
-    {
-        key: "weight",
-        parentKey: "levelNodeSign",
-        enumData: [
-		[0, 'Regular'],
-		[1, 'Light'],
-		[2, 'Semibold'],
-		[3, 'Bold'],
-		[4, 'Italic'],
-	],
-    },
+		key: 'interpolationType',
+		enumData: [
+			[0, 'Linear'],
+			[1, 'Quadratic Ease In'],
+			[2, 'Quadratic Ease Out'],
+			[3, 'Quadratic Ease In Out'],
+			[4, 'Sinusoidal Ease In'],
+			[5, 'Sinusoidal Ease Out'],
+			[6, 'Sinusoidal Ease In Out'],
+			[7, 'Exponential Ease In'],
+			[8, 'Exponential Ease Out'],
+			[9, 'Exponential Ease In Out'],
+			[10, 'Circular Ease In'],
+			[11, 'Circular Ease Out'],
+			[12, 'Circular Ease In Out'],
+			[13, 'Cubic Ease In'],
+			[14, 'Cubic Ease Out'],
+			[15, 'Cubic Ease In Out'],
+			[16, 'Quartic Ease In'],
+			[17, 'Quartic Ease Out'],
+			[18, 'Quartic Ease In Out'],
+			[19, 'Quintic Ease In'],
+			[20, 'Quintic Ease Out'],
+			[21, 'Quintic Ease In Out'],
+		],
+	},
 	{
-        key: "mode",
-        parentKey: "levelNodeGravity",
-        enumData: [
-		[0, 'Legs'],
-		[1, 'Fling'],
-	],
-    },
-    {
-        key:"type",
-        parentKey:"triggerSourceBasic",
-        enumData: [
-		[0, 'Hand'],
-		[1, 'Head'],
-		[2, 'Grapple'],
-		[3, 'Feet'],
-		[4, 'Block'],
-	],
-    },
+		key: 'weight',
+		parentKey: 'levelNodeSign',
+		enumData: [
+			[0, 'Regular'],
+			[1, 'Light'],
+			[2, 'Semibold'],
+			[3, 'Bold'],
+			[4, 'Italic'],
+		],
+	},
 	{
-        key:"mode",
-        parentKey:"triggerTargetAnimation",
-        enumData: [
-		[0, 'Stop'],
-		[1, 'Start'],
-		[2, 'Toggle'],
-		[3, 'Toggle Reverse'],
-		[4, 'Restart'],
-		[5, 'Reset'],
-	],
-    },
+		key: 'mode',
+		parentKey: 'levelNodeGravity',
+		enumData: [
+			[0, 'Legs'],
+			[1, 'Fling'],
+		],
+	},
 	{
-        key:"mode",
-        parentKey:"triggerTargetSound",
-        enumData: [
-		[0, 'Stop'],
-		[1, 'Start'],
-		[2, 'Toggle'],
-		[3, 'Restart'],
-		[4, 'Reset'],
-	],
-    },
+		key: 'type',
+		parentKey: 'triggerSourceBasic',
+		enumData: [
+			[0, 'Hand'],
+			[1, 'Head'],
+			[2, 'Grapple'],
+			[3, 'Feet'],
+			[4, 'Block'],
+		],
+	},
 	{
-        key:"mode",
-        parentKey:"triggerTargetGASM",
-        enumData: [
-		[0, 'Stop'],
-		[1, 'Start'],
-		[2, 'Toggle'],
-		[3, 'Restart'],
-		[4, 'Reset'],
-	],
-    },
-    {
-        key:"mode",
-        parentKey:"triggerTargets",
-        enumData:[
-		[0, 'On Enter'],
-		[1, 'On Leave'],
-		[2, 'Enter or Leave'],
-		[3, 'None'],
-	],
-    },
-    {
-        key:"waveType",
-        enumData: [
-		[0, 'Square'],
-		[1, 'Sawtooth'],
-		[2, 'Sine'],
-		[3, 'Noise'],
-	],
-    },
-    {
-        key:"type",
-        parentKey:"connections",
-        enumData:[
-		[0, 'Node'],
-		[1, 'Player'],
-	],
-    },
-    {
-        key: "type",
-        parentKey:"levelNodeLight",
-        enumData: [
-		[0, 'Point'],
-		[1, 'Spot'],
-	],
-    },
-    {
-        key:"direction",
-        parentKey:"animations",
-        enumData:[
-		[0, 'Restart'],
-		[1, 'Ping Pong'],
-	],
-    },
-    {
-        key:"interpolation",
-        parentKey:"animations",
-        enumData:[
-		[0, 'No interpolation'],
-		[1, 'Interpolation'],
-	],
-    },
+		key: 'mode',
+		parentKey: 'triggerTargetAnimation',
+		enumData: [
+			[0, 'Stop'],
+			[1, 'Start'],
+			[2, 'Toggle'],
+			[3, 'Toggle Reverse'],
+			[4, 'Restart'],
+			[5, 'Reset'],
+		],
+	},
+	{
+		key: 'mode',
+		parentKey: 'triggerTargetSound',
+		enumData: [
+			[0, 'Stop'],
+			[1, 'Start'],
+			[2, 'Toggle'],
+			[3, 'Restart'],
+			[4, 'Reset'],
+		],
+	},
+	{
+		key: 'mode',
+		parentKey: 'triggerTargetGASM',
+		enumData: [
+			[0, 'Stop'],
+			[1, 'Start'],
+			[2, 'Toggle'],
+			[3, 'Restart'],
+			[4, 'Reset'],
+		],
+	},
+	{
+		key: 'mode',
+		parentKey: 'triggerTargets',
+		enumData: [
+			[0, 'On Enter'],
+			[1, 'On Leave'],
+			[2, 'Enter or Leave'],
+			[3, 'None'],
+		],
+	},
+	{
+		key: 'waveType',
+		enumData: [
+			[0, 'Square'],
+			[1, 'Sawtooth'],
+			[2, 'Sine'],
+			[3, 'Noise'],
+		],
+	},
+	{
+		key: 'type',
+		parentKey: 'connections',
+		enumData: [
+			[0, 'Node'],
+			[1, 'Player'],
+		],
+	},
+	{
+		key: 'type',
+		parentKey: 'levelNodeLight',
+		enumData: [
+			[0, 'Point'],
+			[1, 'Spot'],
+		],
+	},
+	{
+		key: 'direction',
+		parentKey: 'animations',
+		enumData: [
+			[0, 'Restart'],
+			[1, 'Ping Pong'],
+		],
+	},
+	{
+		key: 'interpolation',
+		parentKey: 'animations',
+		enumData: [
+			[0, 'No interpolation'],
+			[1, 'Interpolation'],
+		],
+	},
 ];
 
 const blank_types = [
-    {
-        parentKey:"animations",
-        types: {
-            Animation: {
-                name: "idle",
-                frames: [],
-                direction: 0,
-                speed: 1,
-                interpolation: 0,
-            },
-        },
-    },
-    {
-        parentKey:"frames",
-        types: {
-            Frame: {
-                time: 0,
-                position: { x: 0, y: 0, z: 0 },
-            },
-            NotFrame: {
-                asd:123,
-            },
-        },
-    },
+	{
+		parentKey: 'animations',
+		types: {
+			Animation: {
+				name: 'idle',
+				frames: [],
+				direction: 0,
+				speed: 1,
+				interpolation: 0,
+			},
+		},
+	},
+	{
+		parentKey: 'frames',
+		types: {
+			Frame: {
+				time: 0,
+				position: { x: 0, y: 0, z: 0 },
+				rotation: { x: 0, y: 0, z: 0, w: 0 },
+			},
+		},
+	},
+	{
+		parentKey: 'triggerSources',
+		types: {
+			Basic: {
+				triggerSourceBasic: {
+					type: 0,
+				},
+			},
+			Block: {
+				triggerSourceBlockNames: {
+					names: [],
+				},
+			},
+		},
+	},
+	{
+		parentKey: 'names',
+		types: {
+			String: '',
+		},
+	},
+	{
+		parentKey: 'triggerTargets',
+		types: {
+			Animation: {
+				mode: 0,
+				triggerTargetAnimation: {
+					objectID: 0,
+					animationName: 'idle',
+					loop: false,
+					reverse: false,
+					mode: 3, // toggle reverse
+				},
+			},
+			Sublevel: {
+				mode: 0,
+				triggerTargetSubLevel: {
+					levelIdentifier: '',
+					spawnPoint: 'default',
+				},
+			},
+			Ambience: {
+				mode: 0,
+				triggerTargetAmbience: {
+					skyColor0: { r: 0, g: 0, b: 0, a: 0 },
+					skyColor1: { r: 0, g: 0, b: 0, a: 0 },
+					sunAltitude: 0,
+					sunAzimuth: 0,
+					sunSize: 1,
+					fogDensity: 1,
+					changeDuration: 1,
+					interpolationType: 0,
+					useAdvancedSunSettings: false,
+					sunColor: { r: 0, g: 0, b: 0, a: 0 },
+					sunBrightness: 0,
+					ambientColor: { r: 0, g: 0, b: 0, a: 0 },
+					ambientBrightness: 0,
+				},
+			},
+			Sound: {
+				mode: 0,
+				triggerTargetSound: {
+					objectID: 0,
+					mode: 3,
+					repeat: false,
+				},
+			},
+			Light: {
+				mode: 0,
+				triggerTargetLight: {
+					objectID: 0,
+					color: { r: 0, g: 0, b: 0, a: 0 },
+					range: 0,
+					brightness: 0,
+					fadeDuration: 0,
+				},
+			},
+		},
+	},
 ];
 
 function deSerialize(object) {
@@ -250,29 +347,38 @@ function deSerialize(object) {
 	return object.value;
 }
 
-function serializeToMenu(value, key = 'node', parentKey = null, arrayIndex = null) {
+function serializeToMenu(
+	value,
+	key = 'node',
+	parentKey = null,
+	arrayIndex = null,
+) {
 	const serkey = camelToTitleCase(key);
 
-    let node = {
-        title: serkey,
-        key: key,
-        arrayIndex: arrayIndex,
-    };
+	let node = {
+		title: serkey,
+		key: key,
+		arrayIndex: arrayIndex,
+	};
 
-    // Blank type serialization
-    let selected_blank_type;
-    blank_types.forEach((e) => {
-        if (parentKey!=e.parentKey) return;
-        selected_blank_type = e.types;
-    });
-    if (selected_blank_type) {
-        node.blankTypes = selected_blank_type;
-    }
+	// Blank type serialization
+	let selected_blank_type;
+	blank_types.forEach((e) => {
+		if (parentKey != e.parentKey) return;
+		selected_blank_type = e.types;
+	});
+	if (selected_blank_type) {
+		node.blankTypes = selected_blank_type;
+	}
 
-    // Vector3
-	if (typeof value === 'object' && classify_as.vector3.includes(key) && !node.type) {
+	// Vector3
+	if (
+		typeof value === 'object' &&
+		classify_as.vector3.includes(key) &&
+		!node.type
+	) {
 		node = {
-            ...node,
+			...node,
 			type: 'vector3',
 			value: {
 				x: 0,
@@ -283,10 +389,14 @@ function serializeToMenu(value, key = 'node', parentKey = null, arrayIndex = nul
 		};
 	}
 
-    // Vector4
-	if (typeof value === 'object' && classify_as.vector4.includes(key) && !node.type) {
+	// Vector4
+	if (
+		typeof value === 'object' &&
+		classify_as.vector4.includes(key) &&
+		!node.type
+	) {
 		node = {
-            ...node,
+			...node,
 			type: 'vector4',
 			value: {
 				x: 0,
@@ -298,10 +408,14 @@ function serializeToMenu(value, key = 'node', parentKey = null, arrayIndex = nul
 		};
 	}
 
-    // Min-Max range
-    if (typeof value === 'object' && classify_as.minmax.includes(key) && !node.type) {
+	// Min-Max range
+	if (
+		typeof value === 'object' &&
+		classify_as.minmax.includes(key) &&
+		!node.type
+	) {
 		node = {
-            ...node,
+			...node,
 			type: 'minmax',
 			value: {
 				x: 0,
@@ -311,24 +425,28 @@ function serializeToMenu(value, key = 'node', parentKey = null, arrayIndex = nul
 		};
 	}
 
-    // Color
-	if (typeof value === 'object' && classify_as.color.includes(key) && !node.type) {
+	// Color
+	if (
+		typeof value === 'object' &&
+		classify_as.color.includes(key) &&
+		!node.type
+	) {
 		const col = {
 			r: value.r * 255 || 0,
 			g: value.g * 255 || 0,
 			b: value.b * 255 || 0,
 		};
 		node = {
-            ...node,
+			...node,
 			type: 'color',
 			value: rgbToHex(col.r, col.g, col.b),
 		};
 	}
 
-    // Array
+	// Array
 	if (Array.isArray(value) && !node.type) {
 		node = {
-            ...node,
+			...node,
 			type: 'array',
 			isExpandable: value.length > 0,
 			children: value.map((item, index) =>
@@ -337,48 +455,52 @@ function serializeToMenu(value, key = 'node', parentKey = null, arrayIndex = nul
 		};
 	}
 
-    // Objects
+	// Objects
 	if (typeof value === 'object' && value !== null && !node.type) {
 		node = {
-            ...node,
+			...node,
 			type: 'object',
-            arrayIndex: arrayIndex,
+			arrayIndex: arrayIndex,
 			isExpandable: value != {},
 			children: Object.entries(value).map(([subKey, subVal]) =>
-				serializeToMenu(subVal, subKey, arrayIndex!=null ? parentKey : key),
+				serializeToMenu(
+					subVal,
+					subKey,
+					arrayIndex != null ? parentKey : key,
+				),
 			),
 		};
 	}
 
-    // Enums
-    let enumData = null;
-    enumerate_as.forEach((e)=>{
-        if (e.key!=key) return;
-        if (e.parentKey && e.parentKey!=parentKey) return;
-        enumData = e.enumData;
-    });
-    if (enumData && !node.type) {
+	// Enums
+	let enumData = null;
+	enumerate_as.forEach((e) => {
+		if (e.key != key) return;
+		if (e.parentKey && e.parentKey != parentKey) return;
+		enumData = e.enumData;
+	});
+	if (enumData && !node.type) {
 		node = {
-            ...node,
-            type: 'enum',
-            value: value,
-            enumData: enumData,
-        };
-    }
+			...node,
+			type: 'enum',
+			value: value,
+			enumData: enumData,
+		};
+	}
 
-    // Basic data types (number, string, etc)
-    if (!node.type) {
-	    node = {
-            ...node,
-		    type: typeof value,
-		    value: value,
-            arrayIndex: arrayIndex,
-		    isExpandable: false,
-		    children: null,
-	    };
-    }
+	// Basic data types (number, string, etc)
+	if (!node.type) {
+		node = {
+			...node,
+			type: typeof value,
+			value: value,
+			arrayIndex: arrayIndex,
+			isExpandable: false,
+			children: null,
+		};
+	}
 
-    return node;
+	return node;
 }
 
 export { deSerialize, serializeToMenu };
